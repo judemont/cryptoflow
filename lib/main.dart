30704e170c43c0d9b3c73c3e22766ddb205b580c
JdM <julien@rmbi.ch>
2025-04-15 11:45:47 +0200

    i try to fix but idk wtf im doing

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,29 @@ import 'package:cryptoflow/screens/home.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:adwaita/adwaita.dart';
+import 'dart:ui';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return ValueListenableBuilder<ThemeMode>(
-    // valueListenable: themeNotifier,
-    //       builder: (_, ThemeMode currentMode, __) {
-    return MaterialApp(
-      // scrollBehavior: const MaterialScrollBehavior().copyWith(
-      //     dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch}),
-      theme: AdwaitaThemeData.light(),
-      darkTheme: AdwaitaThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-      // themeMode: currentMode
+    return ScrollConfiguration(
+      behavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.trackpad,
+        },
+      ),
+      child: MaterialApp(
+        theme: AdwaitaThemeData.light(),
+        darkTheme: AdwaitaThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
     );
   }
 }
